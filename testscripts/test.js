67a8ca3f63d8408759428edc943baef90f2d63b7
SaschaNaz <saschanaz@outlook.com>
2014-07-21 17:22:58 +0900

    simple test scripts

---
+++ testscripts/test.js
@@ -0,0 +1,10 @@
+var a = new Blob(["abcdefghijklmnopqrstuvxyz1234567890"],  {type: "text/plain;charset=utf-8"});
+var source = new Streams.BlobSourceBuffer(a);
+source._sliceSize = 3;
+source._countercurrent.push(0, 3, 6);
+source.produce(4).then(function (result) { window.result = result; console.log(result) }, function (error) { console.log(error) });
+
+var a = new Blob(["에피르멍청이"],  {type: "text/plain;charset=utf-8"});
+var stream = new BlobStream(a);
+stream.readBytesAs = "text";
+stream.readBytes(4).then(function (result) { window.result = result; console.log(result) }, function (error) { console.log(error) });
