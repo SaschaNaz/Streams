2eff082b7e5d0d22edcc19686a639a8bb264114b
SaschaNaz <saschanaz@outlook.com>
2014-07-22 00:08:35 +0900

    test for fork and slice

--- testscripts/test.js
+++ testscripts/test.js
@@ -1,4 +1,4 @@
-var a = new Blob(["abcdefghijklmnopqrstuvxyz1234567890"],  {type: "text/plain;charset=utf-8"});
+﻿var a = new Blob(["abcdefghijklmnopqrstuvxyz1234567890"],  {type: "text/plain;charset=utf-8"});
 var source = new Streams.BlobSourceBuffer(a);
 source._sliceSize = 3;
 source._countercurrent.push(0, 3, 6);
@@ -8,3 +8,9 @@ var a = new Blob(["에피르멍청이"],  {type: "text/plain;charset=utf-8"});
 var stream = new BlobStream(a);
 stream.readBytesAs = "text";
 stream.readBytes(4).then(function (result) { window.result = result; console.log(result) }, function (error) { console.log(error) });
+
+var forked = stream.fork();
+forked.readBytes(4).then(function (result) { window.result = result; console.log(result) }, function (error) { console.log(error) });
+
+var sliced = stream.slice(2);
+sliced.readBytes(4).then(function (result) { window.result = result; console.log(result) }, function (error) { console.log(error) });
